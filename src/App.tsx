e16a9ca8fe14c38913cc4cd6bcc44eb9fc85e085
jdjaureguim <jdjaureguim@hecaa.com>
2024-10-30 22:45:06 -0500

    Proyecto inicial listo

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { Navigation } from './routes/Navigation'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Navigation />
+    </>
+  )
+}
+
+export default App
